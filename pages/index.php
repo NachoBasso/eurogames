95fc8be2a08792c685d428a4de40a218f3b8827a
NachoBasso <nachobasso@hotmail.com>
2024-06-07 13:21:28 +0200

    Modificación de style.css e index.php para identificar las badge de precios de las del carrito.

--- pages/index.php
+++ pages/index.php
@@ -114,7 +114,7 @@ if (isset($_POST['agregar_al_carrito'])) {
             <div class="carousel-inner">
               <?php foreach ($listadoJuegos as $key => $juego) : ?>
                 <div class="carousel-item <?php echo $key === 0 ? 'active' : ''; ?>">
-                <span class="badge text-black"><?php echo "€". $juego['precio']; ?></span>  
+                <span class="badge-precio text-black"><?php echo "€". $juego['precio']; ?></span>  
               <svg xmlns="http://www.w3.org/2000/svg" class="badge-certificate "  preserveAspectRatio="none" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M211 7.3C205 1 196-1.4 187.6 .8s-14.9 8.9-17.1 17.3L154.7 80.6l-62-17.5c-8.4-2.4-17.4 0-23.5 6.1s-8.5 15.1-6.1 23.5l17.5 62L18.1 170.6c-8.4 2.1-15 8.7-17.3 17.1S1 205 7.3 211l46.2 45L7.3 301C1 307-1.4 316 .8 324.4s8.9 14.9 17.3 17.1l62.5 15.8-17.5 62c-2.4 8.4 0 17.4 6.1 23.5s15.1 8.5 23.5 6.1l62-17.5 15.8 62.5c2.1 8.4 8.7 15 17.1 17.3s17.3-.2 23.4-6.4l45-46.2 45 46.2c6.1 6.2 15 8.7 23.4 6.4s14.9-8.9 17.1-17.3l15.8-62.5 62 17.5c8.4 2.4 17.4 0 23.5-6.1s8.5-15.1 6.1-23.5l-17.5-62 62.5-15.8c8.4-2.1 15-8.7 17.3-17.1s-.2-17.4-6.4-23.4l-46.2-45 46.2-45c6.2-6.1 8.7-15 6.4-23.4s-8.9-14.9-17.3-17.1l-62.5-15.8 17.5-62c2.4-8.4 0-17.4-6.1-23.5s-15.1-8.5-23.5-6.1l-62 17.5L341.4 18.1c-2.1-8.4-8.7-15-17.1-17.3S307 1 301 7.3L256 53.5 211 7.3z"/></svg>
                   
                   <h2 class="card-title text-center text-shadow mb-3 mt-3 p-5"><?php echo $juego['nombre_juego']; ?></h2>
@@ -185,7 +185,7 @@ if (isset($_POST['agregar_al_carrito'])) {
             <div class="carousel-inner">
               <?php foreach ($juegosOferta as $key => $ofertas) : ?>
                 <div class="carousel-item <?php echo $key === 0 ? 'active' : ''; ?> ">
-                <span class="badge text-black"><?php echo "€". $ofertas['precio']; ?></span>  
+                <span class="badge-precio text-black"><?php echo "€". $ofertas['precio']; ?></span>  
               <svg xmlns="http://www.w3.org/2000/svg" class="badge-certificate " viewBox="0 0 512 512"  preserveAspectRatio="none"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M211 7.3C205 1 196-1.4 187.6 .8s-14.9 8.9-17.1 17.3L154.7 80.6l-62-17.5c-8.4-2.4-17.4 0-23.5 6.1s-8.5 15.1-6.1 23.5l17.5 62L18.1 170.6c-8.4 2.1-15 8.7-17.3 17.1S1 205 7.3 211l46.2 45L7.3 301C1 307-1.4 316 .8 324.4s8.9 14.9 17.3 17.1l62.5 15.8-17.5 62c-2.4 8.4 0 17.4 6.1 23.5s15.1 8.5 23.5 6.1l62-17.5 15.8 62.5c2.1 8.4 8.7 15 17.1 17.3s17.3-.2 23.4-6.4l45-46.2 45 46.2c6.1 6.2 15 8.7 23.4 6.4s14.9-8.9 17.1-17.3l15.8-62.5 62 17.5c8.4 2.4 17.4 0 23.5-6.1s8.5-15.1 6.1-23.5l-17.5-62 62.5-15.8c8.4-2.1 15-8.7 17.3-17.1s-.2-17.4-6.4-23.4l-46.2-45 46.2-45c6.2-6.1 8.7-15 6.4-23.4s-8.9-14.9-17.3-17.1l-62.5-15.8 17.5-62c2.4-8.4 0-17.4-6.1-23.5s-15.1-8.5-23.5-6.1l-62 17.5L341.4 18.1c-2.1-8.4-8.7-15-17.1-17.3S307 1 301 7.3L256 53.5 211 7.3z"/></svg>
                   <img src="<?php echo $ofertas['foto']; ?>" class="d-flex  w-50 h-25 justify-content-center mx-auto" alt="<?php echo $ofertas['nombre_juego']; ?>">
                   <div class="d-flex justify-content-center mt-3">
@@ -216,7 +216,7 @@ if (isset($_POST['agregar_al_carrito'])) {
             <div class="carousel-inner"> 
               <?php foreach ($juegosNovedades as $key => $novedades) : ?>
                 <div class="carousel-item <?php echo $key === 0 ? 'active' : ''; ?> ">
-                <span class="badge text-black "><?php echo "€". $novedades['precio']; ?></span>  
+                <span class="badge-precio text-black "><?php echo "€". $novedades['precio']; ?></span>  
               <svg xmlns="http://www.w3.org/2000/svg" class="badge-certificate "   preserveAspectRatio="none" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M211 7.3C205 1 196-1.4 187.6 .8s-14.9 8.9-17.1 17.3L154.7 80.6l-62-17.5c-8.4-2.4-17.4 0-23.5 6.1s-8.5 15.1-6.1 23.5l17.5 62L18.1 170.6c-8.4 2.1-15 8.7-17.3 17.1S1 205 7.3 211l46.2 45L7.3 301C1 307-1.4 316 .8 324.4s8.9 14.9 17.3 17.1l62.5 15.8-17.5 62c-2.4 8.4 0 17.4 6.1 23.5s15.1 8.5 23.5 6.1l62-17.5 15.8 62.5c2.1 8.4 8.7 15 17.1 17.3s17.3-.2 23.4-6.4l45-46.2 45 46.2c6.1 6.2 15 8.7 23.4 6.4s14.9-8.9 17.1-17.3l15.8-62.5 62 17.5c8.4 2.4 17.4 0 23.5-6.1s8.5-15.1 6.1-23.5l-17.5-62 62.5-15.8c8.4-2.1 15-8.7 17.3-17.1s-.2-17.4-6.4-23.4l-46.2-45 46.2-45c6.2-6.1 8.7-15 6.4-23.4s-8.9-14.9-17.3-17.1l-62.5-15.8 17.5-62c2.4-8.4 0-17.4-6.1-23.5s-15.1-8.5-23.5-6.1l-62 17.5L341.4 18.1c-2.1-8.4-8.7-15-17.1-17.3S307 1 301 7.3L256 53.5 211 7.3z"/></svg>
                  
                   <img src="<?php echo $novedades['foto']; ?>" class="d-flex  w-50 h-25 justify-content-center mx-auto" alt="<?php echo $novedades['nombre_juego']; ?>">
